4247331497d25b5a371ff0449230a546d4013dab
keshan <keshan011@live.com>
2020-08-28 23:18:16 +0530

    test added

---
+++ src/extension/parsers/babel.ts
@@ -0,0 +1,18 @@
+import * as parser from '@babel/parser';
+
+export function parse(code: string, languageId: string) {
+  const isTS = languageId.includes('typescript');
+  const plugins: parser.ParserPlugin[] = ['jsx', 'classProperties'];
+
+  if (isTS) {
+    plugins.push('typescript');
+    plugins.push(['decorators', { decoratorsBeforeExport: true }]);
+  } else {
+    plugins.push('flow');
+  }
+
+  return parser.parse(code, {
+    sourceType: 'module',
+    plugins,
+  });
+}
